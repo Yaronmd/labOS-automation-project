18405af7b97c8132e758d8accab7390b9cb2dbc1
Yaronmd <yaronmord@gmail.com>
2025-02-27 10:33:12 +0200

    add more functions to OrderPage

--- cypress/pages/OrderPage.ts
+++ cypress/pages/OrderPage.ts
@@ -3,12 +3,19 @@ import 'cypress-xpath';
 
 class OrderPage {
 
-    private facilityInputPath = "//*[text()='Facility']//ancestor::label//parent::div//input"
-    private physicianInputPath = "//*[text()='Physician']//ancestor::label//parent::div//input"
-
+    private static readonly FACILITY = "Facility";
+    private static readonly PHYSICIAN = "Physician";
+    private static readonly PATIENT = "Patient by ID, first name, last name"
+    
+    private getFieliedInputPath(by_input:string){
+        return `//*[text()='${by_input}']//ancestor::label//parent::div//input`
+    }
     private searchFacility(input:string){
 
-        cy.xpath(this.facilityInputPath).type(input)
+        cy.xpath(this.getFieliedInputPath(OrderPage.FACILITY)).type(input)
+    }
+    private searchPatient(input:string){
+        cy.xpath(this.getFieliedInputPath(OrderPage.PATIENT)).type(input)
     }
     private selectFromDropdown(input:string){
         const path = `//span[text()='${input}']`
@@ -17,10 +24,15 @@ class OrderPage {
     searchAndSelectFacility(input:string){
         this.searchFacility(input)
         this.selectFromDropdown(input)
-    }
     
+    }
+    searchAndSelectPatient(input:string){
+        this.searchPatient(input)
+        this.selectFromDropdown(input)
+    }
+
     validatePhysicianLoadedWithText(input:string){
-        cy.xpath(this.physicianInputPath)
+        cy.xpath(this.getFieliedInputPath(OrderPage.PHYSICIAN))
           .invoke('val')  // Get the value of the input field
           .should('equal', input)  // Assert the value is the one you're expecting
           .then((text) => {
@@ -28,7 +40,7 @@ class OrderPage {
           });
     }
     
-
+    
   }
   
   export default new OrderPage();
