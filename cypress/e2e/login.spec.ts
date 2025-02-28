f82ad7f76d7939b134a84d4928f11a12c9e0b0b2
Yaronmd <yaronmord@gmail.com>
2025-02-28 14:51:25 +0200

    add Login Functionality test

---
+++ cypress/e2e/login.spec.ts
@@ -0,0 +1,39 @@
+import LoginPage from "@pages/LoginPage";
+import { it } from "mocha";
+
+
+describe("Login Functionality", () => {
+    beforeEach(() => {
+        LoginPage.visit();
+      });
+
+    it("should display warning messages when login is attempted without credentials",()=>{
+
+        LoginPage.clickLogin()
+        LoginPage.getListofWarningMessages().then((listOfWarnings) => {
+          // Validate that the warning messages array matches the expected messages
+          expect(listOfWarnings).to.deep.equal([
+              "Username is required", 
+              "Password is required"
+          ]);
+      });
+
+    });
+    it("should display error message when login is attempted with wrong credentials",()=>{
+
+        LoginPage.login("123","123")
+        LoginPage.getListofWarningMessages().then((listOfWarnings) => {
+          // Validate that the warning messages array matches the expected messages
+          expect(listOfWarnings).to.deep.equal([
+              "Incorrect username or password. Please try again.", 
+              
+          ]);
+      });
+
+    });
+
+
+    });
+
+
+
